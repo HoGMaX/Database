5fa72d9e9428d6c3c2707374717396b5a0214671
Max Hoha <shogmax.git@gmail.com>
2020-11-06 12:15:49 +0200

    First Commit

---
+++ Databases/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Databases
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(217, 373);
+            this.panel1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(211, 326);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(3, 335);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "PARSING";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(241, 392);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Parsing";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Databases/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Collections;
+
+namespace Databases
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //List<Exchange> parthlist = new List<Exchange>();
+            String link_XML = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange";
+            XmlDocument doc = new XmlDocument();
+            doc.Load(link_XML);
+            Exchange ex = new Exchange();
+
+
+            foreach (XmlNode node in doc.DocumentElement)
+            {
+               foreach (XmlNode childe in node.ChildNodes)
+                {
+                   if(childe.Name == "txt")
+                    {
+                        ex.txt = childe.InnerText;
+                    }
+                   if(childe.Name == "rate")
+                    {
+                        ex.rate = childe.InnerText; 
+                    }
+                    if (childe.Name == "cc")
+                    {
+                        ex.cc = childe.InnerText;
+                    }
+                    if (childe.Name == "exchangedate")
+                    {
+                        ex.exchangedate = childe.InnerText;
+                    }
+                    //parthlist.Add(ex);
+                }
+                if (ex.txt == "Долар США")
+                {
+                    textBox1.Text += ex.txt + Environment.NewLine;
+                    textBox1.Text += ex.rate + Environment.NewLine;
+                    textBox1.Text += ex.cc + Environment.NewLine;
+                    textBox1.Text += ex.exchangedate += Environment.NewLine;
+                    textBox1.Text += "**************" + Environment.NewLine;
+
+                }
+                else if (ex.txt == "Євро")
+                {
+                    textBox1.Text += ex.txt + Environment.NewLine;
+                    textBox1.Text += ex.rate + Environment.NewLine;
+                    textBox1.Text += ex.cc + Environment.NewLine;
+                    textBox1.Text += ex.exchangedate += Environment.NewLine;
+                    textBox1.Text += "**************" + Environment.NewLine;
+                }
+                else if (ex.txt == "Російський рубль")
+                {
+                    textBox1.Text += "**************" + Environment.NewLine;
+                    textBox1.Text += ex.txt + Environment.NewLine;
+                    textBox1.Text += ex.rate + Environment.NewLine;
+                    textBox1.Text += ex.cc + Environment.NewLine;
+                    textBox1.Text += ex.exchangedate += Environment.NewLine;
+                    textBox1.Text += "**************" + Environment.NewLine;
+                }
+            }
+
+        }
+
+    }
+}
